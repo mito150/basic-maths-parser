7c4cd58142890208b2f238b63b6bd9a673885570
Mateu Soler <mito150@gmail.com>
2015-05-31 18:50:23 +0200

    Added GPL3 headers

--- EvalExpression.java
+++ EvalExpression.java
@@ -1,10 +1,26 @@
+/*
+EvalExpression Copyright (C) 2015 Inalbyss Technologies
+Mateu S. <mito150@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import java.util.Vector;
 
 public class EvalExpression {
 
     private final static String Err = "NaN";
-
-    // TODO: cut the decimals of the result: 2.045125 vs 2.0451250000000005
 
     /**
      * String -> String
